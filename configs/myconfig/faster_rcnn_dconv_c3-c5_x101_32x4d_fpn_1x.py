3743fd437d971b6e76740992f1296ea5995d0a56
YeLyuUT <y.lyu@utwente.nl>
2019-08-19 12:00:15 +0200

    Add test dataset for config

--- configs/myconfig/faster_rcnn_dconv_c3-c5_x101_32x4d_fpn_1x.py
+++ configs/myconfig/faster_rcnn_dconv_c3-c5_x101_32x4d_fpn_1x.py
@@ -123,30 +123,32 @@ data = dict(
         flip_ratio=0.5,
         with_mask=False,
         with_crowd=False,
-        with_label=True))
+        with_label=True),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'ImageSets/VID_val.json',
+        img_prefix=data_root+'/Data/VID/val',
+        img_scale=(1333, 800),
+        img_norm_cfg=img_norm_cfg,
+        size_divisor=32,
+        flip_ratio=0,
+        with_mask=False,
+        with_label=False,
+        test_mode=True),
+        )
 '''
 val=dict(
     type=dataset_type,
-    ann_file=data_root + 'annotations/instances_val2017.json',
-    img_prefix=data_root + 'val2017/',
+    ann_file=data_root + 'ImageSets/VID_val.json',
+    img_prefix=data_root,
     img_scale=(1333, 800),
     img_norm_cfg=img_norm_cfg,
     size_divisor=32,
     flip_ratio=0,
     with_mask=False,
-    with_crowd=True,
+    with_crowd=False,
     with_label=True),
-test=dict(
-    type=dataset_type,
-    ann_file=data_root + 'annotations/instances_val2017.json',
-    img_prefix=data_root + 'val2017/',
-    img_scale=(1333, 800),
-    img_norm_cfg=img_norm_cfg,
-    size_divisor=32,
-    flip_ratio=0,
-    with_mask=False,
-    with_label=False,
-    test_mode=True))'''
+'''
 
 # optimizer
 optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
