ad52f117448b23274c6b652a0cda673abc5712d1
YeLyuUT <y.lyu@utwente.nl>
2019-08-28 21:07:51 +0200

    Add rfcn to package

--- mmdet/models/detectors/__init__.py
+++ mmdet/models/detectors/__init__.py
@@ -12,9 +12,10 @@ from .retinanet import RetinaNet
 from .fcos import FCOS
 from .grid_rcnn import GridRCNN
 from .mask_scoring_rcnn import MaskScoringRCNN
+from .rfcn import RFCN
 
 __all__ = [
     'BaseDetector', 'SingleStageDetector', 'TwoStageDetector', 'RPN',
     'FastRCNN', 'FasterRCNN', 'MaskRCNN', 'CascadeRCNN', 'HybridTaskCascade',
-    'DoubleHeadRCNN', 'RetinaNet', 'FCOS', 'GridRCNN', 'MaskScoringRCNN'
+    'DoubleHeadRCNN', 'RetinaNet', 'FCOS', 'GridRCNN', 'MaskScoringRCNN','RFCN'
 ]
