1189f2684b8f9efbb4a0e57d165d29cba9ee13a3
YeLyuUT <y.lyu@utwente.nl>
2019-10-08 14:17:36 +0200

    Add VIDBlock dataset for imagenet

--- mmdet/datasets/imagenet.py
+++ mmdet/datasets/imagenet.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from .custom import CustomDataset
 from .custom_pair import CustomPairDataset
+from .custom_block import CustomBlockDataset
 from .registry import DATASETS
 
 
@@ -28,6 +29,30 @@ class ImageNetDETVIDDataset(CustomDataset):
             ann['labels'] = np.array(ann['labels'], dtype=np.int64)#.reshape(-1, 1)
         self.img_infos[idx]['ann']=ann
         return ann
+
+
+@DATASETS.register_module
+class ImageNetVIDBlockDataset(CustomBlockDataset):
+  CLASSES = ('airplane', 'antelope', 'bear', 'bicycle', 'bird', 'bus',
+             'car', 'cattle', 'dog', 'domestic_cat', 'elephant', 'fox',
+             'giant_panda', 'hamster', 'horse', 'lion', 'lizard', 'monkey',
+             'motorcycle', 'rabbit', 'red_panda', 'sheep', 'snake', 'squirrel',
+             'tiger', 'train', 'turtle', 'watercraft', 'whale', 'zebra')
+
+  def __init__(self, *args, **kargs):
+    super().__init__(*args, **kargs)
+    self.img_ids = list(range(len(self.img_infos)))
+    self.cat_ids = list(range(len(self.CLASSES)))
+
+  def get_ann_info(self, idx):
+    ann = self.img_infos[idx]['ann']
+    # modify type if necessary.
+    if not isinstance(ann['bboxes'], np.ndarray):
+      ann['bboxes'] = np.array(ann['bboxes'], dtype=np.float32).reshape(-1, 4)
+    if not isinstance(ann['labels'], np.ndarray):
+      ann['labels'] = np.array(ann['labels'], dtype=np.int64)  # .reshape(-1, 1)
+    self.img_infos[idx]['ann'] = ann
+    return ann
 
 
 @DATASETS.register_module
