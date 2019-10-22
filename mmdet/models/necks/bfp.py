ccd5729f1ab7a915593f0625589936e1968ed0da
YeLyuUT <y.lyu@utwente.nl>
2019-10-22 03:22:15 +0200

    Fix training issue in rfcnbfp

--- mmdet/models/necks/bfp.py
+++ mmdet/models/necks/bfp.py
@@ -142,38 +142,10 @@ class FRCNBFP(nn.Module):
         self.refine_type = refine_type
         self.out_channels = out_channels
         assert 0 <= self.refine_level < self.num_levels
-
-        self.ConvModuleList = \
-            [ConvModule(self.in_channels[i],
-                        (i+1)*49,
-                        1,
-                        padding=0,
-                        conv_cfg=self.conv_cfg,
-                        norm_cfg=self.norm_cfg).cuda() for i in range(len(self.in_channels))]
-
-        if self.output_single_lvl:
-            self.refine = ConvModule(
-                sum(self.in_channels),
-                self.out_channels,
-                3,
-                padding=1,
-                conv_cfg=self.conv_cfg,
-                norm_cfg=self.norm_cfg)
-        elif self.refine_type == 'conv':
-            self.refine = ConvModule(
-                self.in_channels,
-                self.in_channels,
-                3,
-                padding=1,
-                conv_cfg=self.conv_cfg,
-                norm_cfg=self.norm_cfg)
-        elif self.refine_type == 'non_local':
-            self.refine = NonLocal2D(
-                self.in_channels,
-                reduction=1,
-                use_scale=False,
-                conv_cfg=self.conv_cfg,
-                norm_cfg=self.norm_cfg)
+        self.ConvModuleList = nn.ModuleList([nn.Conv2d(self.in_channels[i], (i+1)*49, 1).cuda() for i in range(len(self.in_channels))])
+        for m in self.ConvModuleList:
+            if isinstance(m, nn.Conv2d):
+                xavier_init(m, distribution='uniform')
 
     def init_weights(self):
         for m in self.modules():
@@ -195,25 +167,7 @@ class FRCNBFP(nn.Module):
                     inputs[i], size=gather_size, mode='nearest')
             feats.append(gathered)
 
-        if not self.output_single_lvl:
-            # step 2: refine gathered features
-            bsf = sum(feats) / len(feats)
-            if self.refine_type is not None:
-                bsf = self.refine(bsf)
-            # step 3: scatter refined features to multi-levels by a residual path
-            outs = []
-            for i in range(self.num_levels):
-                out_size = inputs[i].size()[2:]
-                if i < self.refine_level:
-                    residual = F.interpolate(bsf, size=out_size, mode='nearest')
-                else:
-                    residual = F.adaptive_max_pool2d(bsf, output_size=out_size)
-                outs.append(residual + inputs[i])
-            return tuple(outs)
-        else:
-            for i in range(len(feats)):
-                feats[i] = self.ConvModuleList[i](feats[i])
-            feats = torch.cat(feats, dim=1)
-            if self.refine_type is not None:
-                feats = [self.refine(feats)]
-            return [feats]
+        for i in range(len(feats)):
+            feats[i] = self.ConvModuleList[i](feats[i])
+        feats = torch.cat(feats, dim=1)
+        return [feats]
